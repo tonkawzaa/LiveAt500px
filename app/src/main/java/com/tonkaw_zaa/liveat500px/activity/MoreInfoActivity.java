a5932936436b2c113dec60f45840bf1d429afa7b
DESKTOP-59QGTIT\Tonkaw_Zaa <tonkaw.zaa@gmail.com>
2017-05-15 21:21:03 +0700

    setUpButton

--- app/src/main/java/com/tonkaw_zaa/liveat500px/activity/MoreInfoActivity.java
+++ app/src/main/java/com/tonkaw_zaa/liveat500px/activity/MoreInfoActivity.java
@@ -2,6 +2,7 @@ package com.tonkaw_zaa.liveat500px.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.tonkaw_zaa.liveat500px.R;
 import com.tonkaw_zaa.liveat500px.fragment.MoreInfoFragment;
@@ -12,11 +13,28 @@ public class MoreInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+        
+        initInstances();
 
         if(savedInstanceState ==null){
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentContainer, MoreInfoFragment.newInstance())
                     .commit();
         }
+    }
+
+    private void initInstances() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
